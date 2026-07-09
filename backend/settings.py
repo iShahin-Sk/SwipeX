69dfbb1123dd0ccfdf6285f2cc99f39197426f06
iShahin-Sk <shahinshaik281@gmail.com>
2026-07-09 23:27:16 +0530

    Configure PostgreSQL database

--- backend/settings.py
+++ backend/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "swipex_db",
+        "USER": "postgres",
+        "PASSWORD": "shannu",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
